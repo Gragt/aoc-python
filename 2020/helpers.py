8161d75f2b79f002ae2fd6e8ce9567dde56c71fe
François Grandjean <francois.grandjean@gmail.com>
2020-12-12 16:26:45 -0600

    Simplify linereader helper function

--- 2020/helpers.py
+++ 2020/helpers.py
@@ -1,4 +1,3 @@
 def linereader(file):
     file = open(file, "r")
-    lines = [line.rstrip("\n") for line in file.readlines()]
-    return tuple(lines)
+    return tuple([line.rstrip("\n") for line in file.readlines()])
